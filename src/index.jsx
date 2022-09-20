443f5b8711b72cbcf0a807c262f0f73b8c8c8cfa
gihonhon <agunggihon@gmail.com>
2022-09-20 23:20:07 +0800

    tabble todo

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/index.jsx'
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './components/App/App'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
